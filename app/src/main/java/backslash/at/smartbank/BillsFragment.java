4ab01e0b4dacc78ec2d9da2fad7f72c68af66405
Christopher Hummel <christopher.hummel@edu.htl-villach.at>
2018-09-22 09:26:26 +0200

    Add fragments

--- app/src/main/java/backslash/at/smartbank/BillsFragment.java
+++ app/src/main/java/backslash/at/smartbank/BillsFragment.java
@@ -1,14 +1,19 @@
 package backslash.at.smartbank;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class BillsFragment extends Fragment {
+
+    static final int REQUEST_IMAGE_CAPTURE = 3113;
+
     public static BillsFragment newInstance() {
         BillsFragment fragment = new BillsFragment();
         return fragment;
@@ -16,11 +21,20 @@ public class BillsFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        dispatchTakePictureIntent();
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_bills, container, false);
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
