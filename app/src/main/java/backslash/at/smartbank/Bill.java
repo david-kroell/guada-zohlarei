9191e57c39156526e01b99c975cd50da0a6c2d95
Christopher Hummel <christopher.hummel@edu.htl-villach.at>
2018-09-22 23:01:56 +0200

    more work

--- app/src/main/java/backslash/at/smartbank/Bill.java
+++ app/src/main/java/backslash/at/smartbank/Bill.java
@@ -1,11 +1,41 @@
 package backslash.at.smartbank;
 
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import org.apache.commons.codec.binary.Base64;
+
 public class Bill {
     public int Id;
     public String Description;
     public Double Price;
     public String Image;
     public String Title;
+
+    public Bill(String Title, Bitmap Image, Double Price) {
+        this.Title = Title;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Image.compress(Bitmap.CompressFormat.PNG, 50, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        Base64.encodeBase64(byteArray);
+        this.Image = new String(Base64.encodeBase64(byteArray));
+        this.Price = Price;
+        this.Description = "";
+    }
+
+    public Bill(String Title, Bitmap Image, Double Price, String Description) {
+        this.Title = Title;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        Base64.encodeBase64(byteArray);
+        this.Image = new String(Base64.encodeBase64(byteArray));
+        this.Price = Price;
+        this.Description = Description;
+    }
 
     public String getDescription() {
         return Description;
