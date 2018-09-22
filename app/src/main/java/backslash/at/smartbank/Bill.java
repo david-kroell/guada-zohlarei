e925a050a54b324ad7fe840a310aec2f5ee50c4a
Christian Gubesch <gubeschc@edu.htl-villach.at>
2018-09-22 20:15:45 +0200

    bills

--- app/src/main/java/backslash/at/smartbank/Bill.java
+++ app/src/main/java/backslash/at/smartbank/Bill.java
@@ -6,4 +6,20 @@ public class Bill {
     public Double Price;
     public String Image;
     public String Title;
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public Double getPrice() {
+        return Price;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
 }
